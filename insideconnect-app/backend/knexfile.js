2dc3ad3c7e239222783542b776ebf84938b56933
EngineerShawn <147225693+EngineerShawn@users.noreply.github.com>
2025-06-09 19:30:59 -0500

    backend init and setup

---
+++ insideconnect-app/backend/knexfile.js
@@ -0,0 +1,41 @@
+// backend/knexfile.js
+require('dotenv').config({ path: './.env' });
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            // Add SSL configuration based on the .env variable
+            ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+        },
+        migrations: {
+            directory: './database/migrations',
+        },
+        seeds: {
+            directory: './database/seeds',
+        }
+    },
+
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL, // Render provides this URL
+        migrations: {
+            directory: './database/migrations',
+        },
+        seeds: {
+            directory: './database/seeds',
+        },
+        pool: {
+            min: 2,
+            max: 10
+        }
+    }
+};
